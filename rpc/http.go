213be449c7d2f5feed41fba9830c30fddf3a8b28
Cobin Bluth <cbluth@mobiquityinc.com>
2021-03-24 17:25:21 +0100

    clean

--- rpc/http.go
+++ rpc/http.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"log"
-	"net"
 	"net/http"
-	"strings"
 )
 
 func HTTPServer() error {
@@ -16,13 +14,3 @@ func HTTPServer() error {
 	return http.ListenAndServe("127.0.0.1:"+connection, mux)
 }
 
-func HTTPServerOLD(conn string) error {
-	conn = strings.TrimPrefix(conn, ":")
-	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:"+conn)
-	if err != nil { return err }
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil { return err }
-	defer l.Close()
-	rServer.Accept(l)
-	return nil
-}
